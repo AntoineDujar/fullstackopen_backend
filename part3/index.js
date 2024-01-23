137f2889d894614cfdbfdd38523ce6fbf8ba30a4
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-01-23 14:33:29 -0500

    3.8

--- part3/index.js
+++ part3/index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 morgan.token('wa', function getId (req, res) { 
     return(JSON.stringify(req.body))
 })
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :wa'))
 
 let persons = [
