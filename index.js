be0ab94eaf1b63366395cc3ac7392b461ec073b0
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-01-24 13:22:02 -0500

    changed structure

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 morgan.token('wa', function getId (req, res) { 
